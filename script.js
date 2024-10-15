bcd10def209a3b7d24d9a8f16756d6c490eee9e4
swedenebomi <swedenebomi@gamil.com>
2024-10-15 17:42:44 +0200

    blaha

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // declaring variables
     const menuIcon = document.getElementById('menu-icon');
     const navbar = document.querySelector('.navbar');
     const showcaseLink = document.getElementById('showcase-link');
 
-    // Define your links
+    // array of objects for each of the links in the navbar
     const links = [
-        { href: "/pages/homepage/index.html", text: "Home", active: true },
-        { href: "/pages/products/products.html", text: "Products" },
-        { href: "/pages/about-us/about-us.html", text: "About Us" },
-        { href: "/pages/contact/contact.html", text: "Contact" },
-        { href: "/pages/references/references.html", text: "References" }
+        { href: "/pages/homepage/index.html", text: "Home",},
+        { href: "/pages/products/products.html", text: "Products", },
+        { href: "/pages/about-us/about-us.html", text: "About Us", },
+        { href: "/pages/contact/contact.html", text: "Contact",  },
+        { href: "/pages/references/references.html", text: "References", }
     ];
 
-    // Function to create and append links to navbar
+    // Get current URL path
+        const currentPath = window.location.pathname;
+
+    // looping through the array and creating html elememts for each of the links
     links.forEach(link => {
         const li = document.createElement('li');
         const a = document.createElement('a');
         a.href = link.href;
         a.textContent = link.text;
-        if (link.active) {
+        
+        // give element class "active" if the href matches the path
+        if (link.href === currentPath) {
             a.classList.add('active');
         }
-        li.appendChild(a);
         navbar.appendChild(li);
+        li.appendChild(a);
+
     });
 
-    // Update showcase link dynamically
+
     showcaseLink.href = "/pages/products/products.html";
 
-    // Menu icon click event to toggle navbar visibility
+
+    // listen for click on the hamburger menu
     menuIcon.addEventListener('click', function () {
-        // Toggle the 'open' class on the navbar and menu icon for responsive design
+
+        //toggle class "open" on navbar and menuIcon
         navbar.classList.toggle('open');
         menuIcon.classList.toggle('open');
 
-        // Optionally, handle transformations or additional styling directly if needed
+
         if (navbar.classList.contains('open')) {
             menuIcon.style.transform = 'rotate(90deg)';
         } else {
